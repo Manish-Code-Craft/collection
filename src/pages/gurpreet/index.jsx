822bee9464807bad960fd6d1264e4c95edae481f
Gurpreet1308 <gurpreet2113singh@gmail.com>
2025-11-26 16:03:10 +0530

    hgtrfg

---
+++ src/pages/gurpreet/index.jsx
@@ -0,0 +1,11 @@
+import styles from './gurpreet.module.css'
+export default function Home() {
+  return (
+    <>
+    <div className={styles.guri}>
+      Gurpreet
+    </div><section></section>
+    se
+    </>
+  );
+}
